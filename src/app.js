50436f42f8a75998d3fe4e984108dfb8e028a1ab
Spdy17 <133760331+Spdy17@users.noreply.github.com>
2023-10-27 20:54:43 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -131,7 +131,7 @@ app.get("/pay", async (req, res) => {
     const paymentUrl = response.data.data.instrumentResponse.redirectInfo.url;
       // console.log(paymentUrl);
          // res.write(paymentUrl);
-      res.redirect("https://www.google.com/")
+      res.redirect("https://webadis.com/")
 
     })
    .catch(function (error) {
